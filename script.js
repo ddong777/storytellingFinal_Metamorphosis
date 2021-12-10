9836cde6d39de4f82e7b7db675c90a63a8a3cb79
ddong777 <39699082+ddong777@users.noreply.github.com>
2021-12-10 19:55:22 +0900

    add file

---
+++ script.js
@@ -0,0 +1,192 @@
+let pinnocchio = [];
+let metamorphosis = [];
+
+function add_p(){
+    pinnocchio.push('피노키오는 주위를 둘러보았다. ');
+    pinnocchio.push('짚으로 만들어진 예전의 오두막집이 아니라, 수수하지만 세련된 장식과 가구들이 있는 아늑하고 예쁜 방이 보였다. 침대 밖으로 뛰쳐나온 피노키오는 그를 위해 마련된 새 옷과 새 모자와 가죽 장화를 발견했다. ');
+    pinnocchio.push('그것들은 피노키오에게 아주 잘 맞았다.<br>');
+    pinnocchio.push('<br>옷을 다 입은 피노키오는 자연스럽게 주머니에 손을 집어 넣었다. 그리고 상아빛의 작은 동전 지갑을 꺼냈다. 지갑 위에는 이런 말이 적혀 있었다. <br>파란 머리의 요정이 사랑하는 피노키오에게 40페니를 돌려준다. <br>그리고 너의 착한 마음씨에 대해 정말로 고맙게 생각한다. <br>피노키오가 지갑을 열어보자 동전 40페니가 아니라 금화 40페니가 있었다. <br>전부 반짝반짝 빛나는 새 금화였다.<br>');
+    pinnocchio.push('<br>피노키오는 자신의 모습을 거울에 비춰 보았는데 마치 다른 사람같았다. ');
+    pinnocchio.push('거울에 비친 피노키오는 더 이상 예전의 꼭두각시 인형이 아니었다. ');
+    pinnocchio.push('대신 거울 속에는 활기있고 총명한 잘생긴 소년의 모습이 보였다. ');
+    pinnocchio.push('밤색 머리에 밝은 하늘색 눈을 가진 소년은 축제날처럼 들떠서 행복해 보였다.<br>');
+    pinnocchio.push('이렇게 놀라운 일들이 계속 일어나자 피노키오는 정말 자기가 잠에서 깨어난 것인지, 아니면 눈을 뜨고 꿈을 꾸고 있는 것인지 알 수가 없었다.<br>');
+    pinnocchio.push('<br>“그런데 아빠는 어디에 계시지?”<br>이렇게 소리치며 옆방으로 달려간 피노키오는 나이가 많이 들어버린 제페토 ―옛날처럼 건강하고 활발하고 명랑한― 를 발견했다.<br>');
+    pinnocchio.push('제페토는 나무를 조각하는 일을 이미 다시 시작하고 있었다. 그래서 나뭇잎과 꽃과 여러 가지 동물 머리로 장식한 예쁜 액자를 만들고 있었다.<br>');
+    pinnocchio.push('“무엇 때문에 갑자기 모든 것이 변한 걸까요? 너무 궁금해요, 말해 주세요, 아빠!” 피노키오는 아빠의 목을 껴안고 입을 맞추며 물었다.<br>');
+    pinnocchio.push('“우리 집이 이렇게 변한 것은 모두 다 네가 한 일이란다.” 제페토가 말했다.<br>');
+    pinnocchio.push('“왜, 내가 한 일이지요?” <br>“아이들이 나쁜 행동을 하다가 착한 일을 하게 되면 그것들은 무언가를 만들어내는 힘을 가지게 되어 가족들에게 좋은 소식을 가져다 주게 된단다.”<br>');
+    pinnocchio.push('“그런데 나무로 만들어진 예전의 피노키오는 어디 숨어 있는 것이지요?”<br> “저기 있어.”<br>');
+    pinnocchio.push('의자에 기대어 있는 커다란 꼭두각시를 가리키며 제페토가 대답했다. ');
+    pinnocchio.push('그 인형은 한쪽으로 고개를 돌리고 팔을 달랑달랑 매단 채, 한가운데에 다리를 십자로 꼬고 서 있었는데, 그렇게 서 있을 수 있다는 것이 불가사의해 보였다.<br>');
+    pinnocchio.push('피노키오는 몸을 돌려 꼭두각시 인형을 바라보았다. 그렇게 한동안 인형을 바라본 뒤 아주 만족스럽다는 듯이 중얼거렸다.<br><br>“내가 꼭두각시였을 때 얼마나 우스웠을까! 이제 이렇게 진짜 사람이 되다니, 정말 기쁘다!”<br>');
+}
+
+function add_m(){
+    metamorphosis.push('그는 철갑처럼 단단한 등껍질을 대고 누워 있었다. ');
+    metamorphosis.push('머리를 약간 쳐들어보니 불룩하게 솟은 갈색의 배가 보였고 그 배는 다시 활 모양으로 휜 각질의 칸들로 나뉘어 있었다. 그는 철갑처럼 단단한 등껍질을 대고 누워 있었다. ');
+    metamorphosis.push('이불은 금방이라도 주르륵 미끄러져내릴 듯 둥그런 언덕 같은 배 위에 가까스로 덮여 있었다. ');
+    metamorphosis.push('몸뚱이에 비해 형편없이 가느다란 수많은 다리들은 애처롭게 버둥거리며 그의 눈앞에서 어른거렸다.<br>');
+    metamorphosis.push('<br>‘이게 대체 어찌된 일일까?’ 그는 생각했다.<br>');
+    metamorphosis.push('<br>꿈은 아니었다.<br>');
+    metamorphosis.push('다소 작기는 해도 사람 사는 방으로 손색이 없는 그의 방은 낯익은 사면의 벽들로 둘러싸여 조용히 놓여 있었다.<br>옷감 견본들이 풀어헤쳐진 채 어지럽게 널려 있는 책상 위로는 -잠자는 출장 영업사원이었다- 그가 얼마 전 어느 화보 잡지에서 오려내 금박의 예쁜 액자에 끼워넣은 그림이 걸려 있었다. 모피 모자를 쓰고 모피 목도리를 두른 채 꼿꼿이 앉아 있는 한 여인의 그림이었다. 그림 속의 그녀는 그를 향해 팔뚝을 완전히 가린 두툼한 모피 토시를 쳐들어 보이고 있었다.<br>');
+    metamorphosis.push('<br>그레고르의 시선은 이어서 창 쪽으로 향했다. ');
+    metamorphosis.push('칙칙한 날씨가 그를 온통 울적한 기분에 젖게 했다. ');
+    metamorphosis.push('빗방울이 후둑후둑 창문의 함석판을 두드리는 소리가 들려왔다. ');
+    metamorphosis.push('‘잠을 조금 더 자서 이 어처구니없는 상황을 모두 잊어버리는 게 어떨까?’ 하고 그는 생각했으나 그건 결코 실행할 수 없는 일이었다. 왜냐하면 그는 오른쪽으로 누워 자는 버릇이 있었는데, 지금 상태로는 도저히 그런 자세로 누울 수가 없었기 때문이다. ');
+    metamorphosis.push('그리고 옆구리에서 이때까지 한 번도 느껴보지 못한 가볍고 둔한 통증이 느껴지기 시작하자 그제서야 그는 그러기를 그만두었다.<br>');
+    metamorphosis.push('<br>그는 생각에 잠겼다. ');
+    metamorphosis.push('‘아아, 세상에! 나는 어쩌다 이런 고달픈 직업을 택했단 말인가. 허구한 날 여행만 다녀야 한다니. 회사에 앉아 실제의 업무를 보는 일보다 스트레스가 훨씬 더 심하다. 게다가 여행할 때의 이런저런 피곤한 일들이 마음을 더 무겁게 한다. 기차를 제대로 갈아타기 위해 늘 신경을 써야 하는 일, 불규칙하고 형편없는 식사, 상대가 늘 바뀌어 결코 오래갈 수 없는 만남과 결코 진실하게 이루어질 수 없는 인간적 교류 등등.<br>');
+    metamorphosis.push('<br>악마여, 제발 좀 이 모든 것들을 다 가져가다오.<br>');
+    metamorphosis.push('<br>배 위쪽이 약간 가려운 느낌이 들었다. ');
+    metamorphosis.push('머리를 좀더 쳐들어 더 잘 볼 수 있도록 그는 등으로 몸을 밀어 천천히 침대 기둥 쪽으로 다가갔다. 그러다 마침내 가려운 곳을 발견했는데 그곳은 온통, 뭐라고 판단하기 어려운 깨알같이 작고 흰 반점들로 뒤덮여 있었다. ');
+    metamorphosis.push('그는 다리 하나를 내밀어 그곳을 만져보려고 했으나 금세 다리를 뒤로 움츠려야 했다. 그 부위에 다리가 닿자마자 오싹하는 냉기가 그의 온 몸을 휘감았기 때문이다. ');
+    
+}
+
+function random(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+const text = document.querySelector(".block1");
+const button = document.querySelector(".block2");
+
+setup();
+draw();
+
+function setup(){
+    add_p();
+    add_m();   
+    console.log(pinnocchio.length); 
+    console.log(metamorphosis.length); 
+}
+
+function draw(){
+    requestAnimationFrame(draw);
+}
+
+let btnText1 = '한 마리의 흉측한 사람으로 변해 있는 자신의 모습을 발견했다. ';
+let btnText2 = '다른 아이들처럼 갑충으로 변해 있다는 것을 알고 얼마나 놀랐는지 모른다!<br>';
+
+let txt1 = '한 마리의 흉측한 사람으로 변해 있는 자신의 모습을 발견했다. ';
+let txt2 = '다른 아이들처럼 갑충으로 변해 있다는 것을 알고 얼마나 놀랐는지 모른다!<br>';
+
+let index = 0;
+
+function addContent1(){
+    text.innerHTML += txt1;
+
+    let btn1 = document.getElementById('btn1');
+    let btn2 = document.getElementById('btn2');
+
+    btn1.style.textAlign = "left";
+    btn2.style.textAlign = "left";
+
+    if (index < pinnocchio.length-1){
+        index ++;
+
+        btnText1 = pinnocchio[index];
+        btnText2 = metamorphosis[index];
+    } else {
+        btnText1 = '';
+        btnText2 = '';
+    }
+
+    txt1 = btnText1.replace(/피노키오/gi,'--');
+    
+    if (random(0,1) < 0.5 ) txt1 = txt1.replace(/사람/gi,'갑충');
+    else txt1 = txt1.replace(/소년/gi,'갑충');
+
+    if (random(0,1) < 0.2 ) txt1 = txt1.replace(/나무 인형/gi,'출장 영업사원');
+    else if (random(0,1) < 0.4 ) txt1 = txt1.replace(/꼭두각시 인형/gi,'출장 영업사원');
+    else if (random(0,1) < 0.6 ) txt1 = txt1.replace(/꼭두각시/gi,'출장 영업사원');
+    else if (random(0,1) < 0.8 ) txt1 = txt1.replace(/인형/gi,'출장 영업사원');
+
+    if (random(0,1) < 0.1 ) txt1 = txt1.replace(/총명한/gi,'단단한');
+    else if (random(0,1) < 0.2 ) txt1 = txt1.replace(/잘생긴/gi,'단단한');
+    else if (random(0,1) < 0.3 ) txt1 = txt1.replace(/총명한/gi,'둔한');
+    else if (random(0,1) < 0.4 ) txt1 = txt1.replace(/잘생긴/gi,'둔한');
+    else if (random(0,1) < 0.5 ) txt1 = txt1.replace(/총명한/gi,'피곤한');
+    else if (random(0,1) < 0.6 ) txt1 = txt1.replace(/잘생긴/gi,'피곤한');
+
+    txt2 = btnText2.replace(/그래고르 잠자/gi,'--');
+    txt2 = txt2.replace(/그래고르/gi,'--');
+    txt2 = txt2.replace(/잠자/gi,'--');
+
+    if (random(0,1) < 0.5 )txt2 = txt2.replace(/갑충/gi,'사람');
+    else txt2 = txt2.replace(/갑충/gi,'소년');
+
+    if (random(0,1) < 0.2 ) txt2 = txt2.replace(/출장 영업사원/gi,'나무 인형');
+    else if (random(0,1) < 0.4 ) txt2 = txt2.replace(/출장 영업사원/gi,'꼭두각시 인형');
+    else if (random(0,1) < 0.6 ) txt2 = txt2.replace(/출장 영업사원/gi,'꼭두각시');
+    else if (random(0,1) < 0.8 ) txt2 = txt2.replace(/출장 영업사원/gi,'인형');
+
+    if (random(0,1) < 0.1 ) txt2 = txt2.replace(/단단한/gi,'총명한');
+    else if (random(0,1) < 0.2 ) txt2 = txt2.replace(/단단한/gi,'잘생긴');
+    else if (random(0,1) < 0.3 ) txt2 = txt2.replace(/둔한/gi,'총명한');
+    else if (random(0,1) < 0.4 ) txt2 = txt2.replace(/둔한/gi,'잘생긴');
+    else if (random(0,1) < 0.5 ) txt2 = txt2.replace(/피곤한/gi,'총명한');
+    else if (random(0,1) < 0.6 ) txt2 = txt2.replace(/피곤한/gi,'잘생긴');
+    
+    btn1.innerText = txt1.replace(/<br>/gi,'');
+    btn2.innerText = txt2.replace(/<br>/gi,'');
+}
+
+function addContent2(){
+    text.innerHTML += txt2;
+
+    let btn1 = document.getElementById('btn1');
+    let btn2 = document.getElementById('btn2');
+
+    btn1.style.textAlign = "left";
+    btn2.style.textAlign = "left";
+
+    if (index < pinnocchio.length-1){
+        index ++;
+
+        btnText1 = pinnocchio[index];
+        btnText2 = metamorphosis[index];
+    } else {
+        btnText1 = '';
+        btnText2 = '';
+    }
+
+    txt1 = btnText1.replace(/피노키오/gi,'--');
+    
+    if (random(0,1) < 0.5 ) txt1 = txt1.replace(/사람/gi,'갑충');
+    else txt1 = txt1.replace(/소년/gi,'갑충');
+
+    if (random(0,1) < 0.2 ) txt1 = txt1.replace(/나무 인형/gi,'출장 영업사원');
+    else if (random(0,1) < 0.4 ) txt1 = txt1.replace(/꼭두각시 인형/gi,'출장 영업사원');
+    else if (random(0,1) < 0.6 ) txt1 = txt1.replace(/꼭두각시/gi,'출장 영업사원');
+    else if (random(0,1) < 0.8 ) txt1 = txt1.replace(/인형/gi,'출장 영업사원');
+
+    if (random(0,1) < 0.1 ) txt1 = txt1.replace(/총명한/gi,'단단한');
+    else if (random(0,1) < 0.2 ) txt1 = txt1.replace(/잘생긴/gi,'단단한');
+    else if (random(0,1) < 0.3 ) txt1 = txt1.replace(/총명한/gi,'둔한');
+    else if (random(0,1) < 0.4 ) txt1 = txt1.replace(/잘생긴/gi,'둔한');
+    else if (random(0,1) < 0.5 ) txt1 = txt1.replace(/총명한/gi,'피곤한');
+    else if (random(0,1) < 0.6 ) txt1 = txt1.replace(/잘생긴/gi,'피곤한');
+
+    txt2 = btnText2.replace(/그래고르 잠자/gi,'--');
+    txt2 = txt2.replace(/그래고르/gi,'--');
+    txt2 = txt2.replace(/잠자/gi,'--');
+
+    if (random(0,1) < 0.5 )txt2 = txt2.replace(/갑충/gi,'사람');
+    else txt2 = txt2.replace(/갑충/gi,'소년');
+
+    if (random(0,1) < 0.2 ) txt2 = txt2.replace(/출장 영업사원/gi,'나무 인형');
+    else if (random(0,1) < 0.4 ) txt2 = txt2.replace(/출장 영업사원/gi,'꼭두각시 인형');
+    else if (random(0,1) < 0.6 ) txt2 = txt2.replace(/출장 영업사원/gi,'꼭두각시');
+    else if (random(0,1) < 0.8 ) txt2 = txt2.replace(/출장 영업사원/gi,'인형');
+
+    if (random(0,1) < 0.1 ) txt2 = txt2.replace(/단단한/gi,'총명한');
+    else if (random(0,1) < 0.2 ) txt2 = txt2.replace(/단단한/gi,'잘생긴');
+    else if (random(0,1) < 0.3 ) txt2 = txt2.replace(/둔한/gi,'총명한');
+    else if (random(0,1) < 0.4 ) txt2 = txt2.replace(/둔한/gi,'잘생긴');
+    else if (random(0,1) < 0.5 ) txt2 = txt2.replace(/피곤한/gi,'총명한');
+    else if (random(0,1) < 0.6 ) txt2 = txt2.replace(/피곤한/gi,'잘생긴');
+    
+    btn1.innerText = txt1.replace(/<br>/gi,'');
+    btn2.innerText = txt2.replace(/<br>/gi,'');
+}
